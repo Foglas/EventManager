bf00613688838b761e26e0c70ce6c63da41f9648
Matěj Grohmann <grohmannmatejj99@gmail.com>
2023-03-27 02:42:42 +0200

    FE
    comments to login controller

--- src/front_end/dbs_2_front_end/lib/controller/login_controller.dart
+++ src/front_end/dbs_2_front_end/lib/controller/login_controller.dart
@@ -8,11 +8,13 @@ import '../utils/menu_item.dart';
 import '../widgets/validation_text.dart';
 import 'app_controller.dart';
 
+/// Represents the different states for the login screen.
 enum LoginViewState {
-  registration,
-  login,
-  forgotPassword;
+  registration, // Registration form
+  login, // Login form
+  forgotPassword; // Forgot password form
 
+  /// Returns the title of the login screen for the current state.
   String title() {
     switch (this) {
       case LoginViewState.registration:
@@ -24,6 +26,7 @@ enum LoginViewState {
     }
   }
 
+  /// Returns the text to display on the submit button for the current state.
   String submitTextValue() {
     switch (this) {
       case LoginViewState.registration:
@@ -35,65 +38,71 @@ enum LoginViewState {
     }
   }
 
+  /// Returns true if the current state is the login state.
   bool isLogin() {
     return this == LoginViewState.login;
   }
 
+  /// Returns true if the current state is the registration state.
   bool isRegistration() {
     return this == LoginViewState.registration;
   }
 
+  /// Returns true if the current state is the forgot password state.
   bool isResetPassword() {
     return this == LoginViewState.forgotPassword;
   }
 }
 
 class LoginController extends GetxController {
+  // Observable state for the current view state
   Rx<LoginViewState> state = Rx(LoginViewState.login);
 
+  // Controllers for all text fields in the form
   TextEditingController emailTextController = TextEditingController();
-  RxBool emailErrorState = false.obs;
-
   TextEditingController passwordTextController = TextEditingController();
-  RxBool passwordErrorState = false.obs;
-
   TextEditingController passwordAgainTextController = TextEditingController();
-  RxBool passwordAgainErrorState = false.obs;
-
   TextEditingController nameTextController = TextEditingController();
-  RxBool nameErrorState = false.obs;
-
   TextEditingController surnameTextController = TextEditingController();
-  RxBool surnameErrorState = false.obs;
-
   TextEditingController phoneTextController = TextEditingController();
-  RxBool phoneErrorState = false.obs;
-
   TextEditingController dateTextController = TextEditingController();
+
+  // Observable error states for each text field
+  RxBool emailErrorState = false.obs;
+  RxBool passwordErrorState = false.obs;
+  RxBool passwordAgainErrorState = false.obs;
+  RxBool nameErrorState = false.obs;
+  RxBool surnameErrorState = false.obs;
+  RxBool phoneErrorState = false.obs;
   RxBool dateErrorState = false.obs;
 
+  // Observable widget to display success/error messages
   Rx<Widget> messageWidget = Rx(const SizedBox.shrink());
 
-  RxString errorLoginMessage = ''.obs;
-
+  // Default birth date to January 1st, 2000
   DateTime birthDate = DateTime(2000, 1, 1);
 
-  handleSwitchToRegistration() {
+  /// Resets error states and switches to registration view.
+  void handleSwitchToRegistration() {
     _resetErrorStates();
     state.value = LoginViewState.registration;
   }
 
-  handleSwitchToLogin() {
+  /// Resets error states and switches to login view.
+  void handleSwitchToLogin() {
     _resetErrorStates();
     state.value = LoginViewState.login;
   }
 
-  handleSwitchToForgotPassword() {
+  /// Resets error states and switches to forgot password view.
+  void handleSwitchToForgotPassword() {
     _resetErrorStates();
     state.value = LoginViewState.forgotPassword;
   }
 
-  handleSelectDate() async {
+  /// Shows a material date picker dialog to select a birth date.
+  /// Updates the [dateTextController] with the selected date.
+  Future<void> handleSelectDate() async {
     await showMaterialDatePicker(
       title: 'Select date',
       firstDate: DateTime(1910),
@@ -108,7 +117,10 @@ class LoginController extends GetxController {
     );
   }
 
-  handleSubmitButton() async {
+  /// Submits the form based on the current view state.
+  /// Displays a circular progress indicator while processing the submission.
+  /// Does nothing if the view state is not supported.
+  Future<void> handleSubmitButton() async {
     AppController.to.displayCircularProgressIndicator.value = true;
     switch (state.value) {
       case LoginViewState.registration:
@@ -124,9 +136,14 @@ class LoginController extends GetxController {
     AppController.to.displayCircularProgressIndicator.value = false;
   }
 
-  _handleRegistration() async {
+  /// Handles user registration.
+  /// Validates the form and sends a registration request to the server.
+  /// Updates error and success message states accordingly.
+  /// Resets the text editing controllers on success.
+  Future<void> _handleRegistration() async {
     _resetErrorStates();
 
+    // Check if all fields are filled
     if (emailTextController.value.text.isEmpty ||
         dateTextController.value.text.isEmpty ||
         nameTextController.value.text.isEmpty ||
@@ -134,39 +151,43 @@ class LoginController extends GetxController {
         phoneTextController.value.text.isEmpty ||
         passwordTextController.value.text.isEmpty ||
         passwordAgainTextController.value.text.isEmpty) {
+      // Update error states for each empty field
       if (nameTextController.value.text.isEmpty) {
         nameErrorState.value = true;
       }
-
       if (surnameTextController.value.text.isEmpty) {
         surnameErrorState.value = true;
       }
-
       if (phoneTextController.value.text.isEmpty) {
         phoneErrorState.value = true;
       }
-
       if (emailTextController.value.text.isEmpty) {
         emailErrorState.value = true;
       }
-
       if (passwordTextController.value.text.isEmpty) {
         passwordErrorState.value = true;
       }
-
       if (passwordAgainTextController.value.text.isEmpty) {
         passwordAgainErrorState.value = true;
       }
-
       if (dateTextController.value.text.isEmpty) {
         dateErrorState.value = true;
       }
-
+      // Set error message
       _putErrorMessage(
-          message: "All fields has to be filled in order to register");
+          message: "All fields have to be filled in order to register");
       return;
     }
 
+    // Check if email is valid
+    if (!emailTextController.value.text.isEmail) {
+      // Update error state and set error message
+      emailErrorState.value = true;
+      _putErrorMessage(message: "Email is not a valid email");
+      return;
+    }
+
+    // Send registration request to the server
     final ServerResponse response = await RestService.to.registerUser(
       dateOfBirth: dateTextController.value.text,
       email: emailTextController.value.text,
@@ -178,59 +199,137 @@ class LoginController extends GetxController {
       username: emailTextController.value.text,
     );
 
+    // Handle server response
     if (response.statusCode == 400) {
+      // Check error types and update error states accordingly
+      if (response.body['errorType'] == "name_surname_same") {
+        nameErrorState.value = true;
+        surnameErrorState.value = true;
+      }
+      if (response.body['errorType'] == "phone_number_length") {
+        phoneErrorState.value = true;
+      }
+      if (response.body['errorType'] == "password_not_match") {
+        passwordAgainErrorState.value = true;
+      }
+      if (response.body['errorType'] == "email_exists") {
+        emailErrorState.value = true;
+      }
+
+      // Set error message for invalid registration data
       _putErrorMessage(message: response.body['message']);
       return;
     }
-
-    state.value = LoginViewState.login;
-    _putSuccessMessage(message: response.body['message']);
-
-    _resetTextEditingControllers();
+    if (response.statusCode == 200) {
+      // Set success message for successful registration and switch to login view
+      state.value = LoginViewState.login;
+      _putSuccessMessage(message: response.body['message']);
+      // Reset text editing controllers
+      _resetTextEditingControllers();
+      return;
+    }
+    if (response.statusCode == 500) {
+      // Set error message for server error
+      _putErrorMessage(message: response.body['message']);
+    }
   }
 
-  _handleLogin() async {
+  /// Handles user login.
+  /// Validates the form and sends a login request to the server.
+  /// Updates error message and user login state accordingly.
+  /// Resets the text editing controllers on success.
+  Future<void> _handleLogin() async {
+    // Reset error states
     passwordErrorState.value = false;
     emailErrorState.value = false;
 
+    // Check if both email and password are filled
     if (emailTextController.text.isEmpty ||
         passwordTextController.text.isEmpty) {
+      // Update error states and set error message
       emailErrorState.value = true;
       passwordErrorState.value = true;
-
-      _putErrorMessage(message: "Both email and password has to be filled");
+      _putErrorMessage(message: "Both email and password have to be filled");
       return;
     }
 
+    // Check if email is valid
     if (!emailTextController.value.text.isEmail) {
+      // Update error state and set error message
       emailErrorState.value = true;
       _putErrorMessage(message: "Email is not a valid email");
       return;
     }
 
+    // Send login request to the server
     final ServerResponse response = await RestService.to.loginUser(
         password: passwordTextController.value.text,
         email: emailTextController.value.text);
 
+    // Handle server response
     if (response.statusCode == 400) {
+      // Check error types and update error states accordingly
+      if (response.body['errorType'] == "wrong_password") {
+        passwordErrorState.value = true;
+      }
+      if (response.body['errorType'] == "user_not_found") {
+        emailErrorState.value = true;
+      }
+
+      // Set error message for invalid login data
       _putErrorMessage(message: response.body['message']!);
       return;
     }
-
     if (response.statusCode == 200) {
+      // Set user login state and switch to profile view
       AppController.to.isUserLoggedIn.value = true;
       AppController.to.handleMenuItemTapped(MenuItem.profile);
+      // Reset text editing controllers
+      _resetTextEditingControllers();
+      return;
+    }
+    if (response.statusCode == 500) {
+      // Set error message for server error
+      _putErrorMessage(message: response.body['message']);
+    }
+  }
+
+  /// Handles forgot password action.
+  /// Validates the email and switches to login view.
+  /// Sets a success message indicating that the password reset feature is not implemented yet.
+  /// Resets text editing controllers.
+  Future<void> _handleForgotPassword() async {
+    // Reset error state
+    emailErrorState.value = false;
+
+    // Check if email is filled and valid
+    if (emailTextController.value.text.isEmpty) {
+      // Update error state and set error message
+      emailErrorState.value = true;
+      _putErrorMessage(
+          message: 'Email has to be filled in order to reset your password');
+      return;
+    }
+    if (!emailTextController.value.text.isEmail) {
+      // Update error state and set error message
+      emailErrorState.value = true;
+      _putErrorMessage(
+          message:
+              'Email has to be a valid email in order to reset your password');
+      return;
     }
 
-    _resetTextEditingControllers();
-  }
-
-  _handleForgotPassword() async {
+    // Set success message and switch to login view
     state.value = LoginViewState.login;
+    _putSuccessMessage(
+        message:
+            "This feature is not implemented yet. Please contact support for password reset.");
+    // Reset text editing controllers
     _resetTextEditingControllers();
   }
 
-  _resetTextEditingControllers() {
+  /// Resets text editing controllers.
+  void _resetTextEditingControllers() {
     nameTextController.text = '';
     surnameTextController.text = '';
     emailTextController.text = '';
@@ -240,7 +339,8 @@ class LoginController extends GetxController {
     dateTextController.text = '';
   }
 
-  _resetErrorStates() {
+  /// Resets error states.
+  void _resetErrorStates() {
     emailErrorState.value = false;
     passwordErrorState.value = false;
     passwordAgainErrorState.value = false;
@@ -250,11 +350,13 @@ class LoginController extends GetxController {
     dateErrorState.value = false;
   }
 
-  _putErrorMessage({required String message}) {
+  /// Sets error message state with the given message.
+  void _putErrorMessage({required String message}) {
     messageWidget.value = ErrorMessage(message);
   }
 
-  _putSuccessMessage({required String message}) {
+  /// Sets success message state with the given message.
+  void _putSuccessMessage({required String message}) {
     messageWidget.value = SuccessMessage(message);
   }
 }
