a33c2c404452644812b15feea6969be6fa769d8d
Foglas <kubik.fogl@seznam.cz>
2023-04-01 16:09:05 +0200

    Odebrani AuthorizationService a prevedeni logiky rovnou do authorizationFilter

--- src/main/java/cz/uhk/fim/projekt/EventManager/config/AuthorizationFilter.java
+++ src/main/java/cz/uhk/fim/projekt/EventManager/config/AuthorizationFilter.java
@@ -1,6 +1,8 @@
 package cz.uhk.fim.projekt.EventManager.config;
 
-import cz.uhk.fim.projekt.EventManager.service.AuthorizationService;
+import cz.uhk.fim.projekt.EventManager.Domain.User;
+import cz.uhk.fim.projekt.EventManager.service.UserService;
+import cz.uhk.fim.projekt.EventManager.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,11 +18,15 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @Order(1)
 public class AuthorizationFilter extends OncePerRequestFilter {
 
-  private AuthorizationService authService;
+  private JwtUtil jwtUtil;
+
+  private UserService userService;
+
 
   @Autowired
-  public AuthorizationFilter(AuthorizationService authorizationService) {
-    this.authService = authorizationService;
+  public AuthorizationFilter(JwtUtil jwtUtil, UserService userService) {
+    this.jwtUtil = jwtUtil;
+    this.userService = userService;
   }
 
   @Override
@@ -29,6 +35,35 @@ public class AuthorizationFilter extends OncePerRequestFilter {
     HttpServletResponse response,
     FilterChain chain
   ) throws IOException, ServletException {
-    authService.filterRequest(request, response, chain);
+    boolean isAuthApi = request.getServletPath().startsWith("/api/auth/");
+
+    /// no need for jwt token authorization
+    if (!isAuthApi) {
+      chain.doFilter(request, response);
+    }
+
+    try {
+      String header = request.getHeader("Authorization");
+
+      if (header == null || !header.startsWith("Bearer ")) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return;
+      }
+
+      String token = header.replace("Bearer ", "");
+
+      String email = jwtUtil.getEmailFromToken(token);
+
+      User user = userService.findUserByEmail(email);
+
+      if (user == null) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      } else {
+        chain.doFilter(request, response);
+      }
+    } catch (Exception exception) {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
   }
-}
+  }
+
