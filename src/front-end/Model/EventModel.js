c41f23beeeda13a346c4aab52b257e2884fd9d33
Foglas <kubik.fogl@seznam.cz>
2024-02-24 12:12:32 +0100

    urava pred zkouskou na zmenu na server side

--- src/front-end/Model/EventModel.js
+++ src/front-end/Model/EventModel.js
@@ -42,3 +42,32 @@ function getAttendedEvents(){
         return null;
     });
 }
+
+function attendToEvent(e){
+    e.preventDefault();
+    const token = 'Bearer ' + localStorage.getItem('token');
+    console.log("click "+token);
+    const headers = {
+        method: "POST",
+        headers: {
+          'Authorization': token,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "state":"koupeno"
+        })
+      }
+
+    console.log("event id in attendEvent " +e.target.dataset.id);
+    fetch("http://localhost:8080/api/auth/event/"+e.target.dataset.id+"/attend",headers)
+    .then((response) => response.json())
+    .then((data) => 
+    {
+        const button =  e.target;
+        setSuccessButtonAttendStyle(button);
+        console.log(data);
+    })
+    .catch((error) => console.log("There is problem", error));
+    
+
+}
